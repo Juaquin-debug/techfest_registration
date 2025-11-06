dcc89f2e78b54ee367182e17235069e3d8aeb6f1
Valladolid <12413479@benilde.edu.ph>
2025-11-06 12:50:03 +0800

    Enabled Participant Information Collection

--- techfest_registration.py
+++ techfest_registration.py
@@ -1,13 +1,21 @@
 print ("Welcome to SMIT TechFest!")
 print ("Organized by Juaquin Luis T. Valladolid of APPDAET BTCS2\n")
 
-participants = input("How many participants will register?: ")
-if participants <= "0":
+register = input("How many participants will register?: ")
+if register <= "0":
     print("Invalid number of Participants.")
     exit()
 else:
-    participants = int(participants)
+    register = int(register)
 
-for participant in range(participants):
+participants = []
+for i in range(register):
     name = input("Enter Participant Name: ")
     track = input ("Enter Chosen Track: ")
+    participants.append({"name": name,"track": track})
+
+
+print("Registered Participants:")
+
+for p in range(len(participants)):
+    print(f"{p +1}. {participants[p]["name"]} - {participants[p]["track"]}")
